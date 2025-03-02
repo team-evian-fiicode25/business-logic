03b8defdd524cbf9578272ec4335165046c9e204
DESKTOP-RIBNMU0\probb <probbertt@gmail.com>
2025-03-02 16:29:58 +0200

    FII-2: - Remove not used code.

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,22 +1,10 @@
 package handlers
 
 import (
-	"errors"
 	"net/http"
 )
 
 type AuthHandler interface {
 	Register(w http.ResponseWriter, r *http.Request)
 	Login(w http.ResponseWriter, r *http.Request)
-}
-
-func NewAuthHandler(objectType string) (AuthHandler, error) {
-	switch objectType {
-	case "consumer":
-		return &ConsumerAuthHandler{}, nil
-	case "authorities":
-		return &AuthoritiesAuthHandler{}, nil
-	default:
-		return nil, errors.New("invalid auth handler type")
-	}
 }
